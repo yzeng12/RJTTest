16cea9bf50d162d688b9669567dc591a32a773b2
yzeng <yzeng@DESKTOP-V1PN0MG>
2018-09-25 16:32:33 -0500

    0.1

--- AAA/src/module-info.java
+++ AAA/src/module-info.java
@@ -6,4 +6,18 @@
  *
  */
 module AAA {
+	/*
+	public class IncomeTax {
+		
+		public void calculateIT(int income) {
+			int tax = income/10;
+		}
+		
+		
+		
+		public void calculateIT(int income, int luxurySpending) {}
+
+	}
+	*/
+
 }
